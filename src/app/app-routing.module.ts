c61af5862549bcaa8f5b4567918063a33d3229df
AhmedAbdelfattah0 <ahmed.m.abdelfttah@gmail.com>
2020-10-30 19:24:30 +0200

    finalizing the task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { VideoCallComponent } from './video-call/video-call.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:VideoCallComponent},
+  {path:"video-call",component:VideoCallComponent},
+  {path:"products",component:ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoCallComponent } from './video-call/video-call.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoCallComponent,
+    NavbarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
